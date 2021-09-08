2c73c70b759eda7aa9d990bff3c87919195e8296
philomela <romaphilomela@yandex.ru>
2021-09-09 00:39:32 +0300

    instagrabb-01. Added folder for web api and moved files of front in front folder

---
+++ front/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header/Header';
+import Content from './components/Content/Content';
+import Footer from './components/Footer/Footer';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="app-wrapper">
+        <Header/>
+        <Content/>      
+        <Footer/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
