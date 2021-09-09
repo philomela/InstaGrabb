cdbe5d250b9c255a8e5ac69f0ee0445c8d192511
philomela <romaphilomela@yandex.ru>
2021-09-09 20:16:03 +0300

    InstaGrabb-01. Added gitignore and send request front to api

--- webAPI/Startup.cs
+++ webAPI/Startup.cs
@@ -26,7 +26,7 @@ namespace InstaGrabb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -39,10 +39,11 @@ namespace InstaGrabb
         {
             if (env.IsDevelopment())
             {
+                app.UseCors(builder => builder.AllowAnyOrigin());
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "InstaGrabb v1"));
-            }
+            }       
 
             app.UseHttpsRedirection();
 
