43c7665fad1a4220f0988463afc89dc1491b4da8
philomela <romaphilomela@yandex.ru>
2021-09-15 00:59:16 +0300

    InstaGrabb-01. Added new configur section appsetting and added new sql script line for create tables

--- webAPI/Controllers/Instagram.cs
+++ webAPI/Controllers/Instagram.cs
@@ -1,24 +1,61 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using InstaSharper;
+using InstaSharper.API.Builder;
+using InstaSharper.Classes;
+using InstaSharper.Logger;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace InstaGrabb.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class InstagramController : ControllerBase
     {
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<InstagramController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public InstagramController(ILogger<InstagramController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<JsonResult> GetAsync()
         {
-            return null;
+            var delay = RequestDelay.FromSeconds(2, 2);
+
+            var userSession = new UserSessionData
+            {
+                UserName = _configuration.GetSection("Credentials").GetSection("InstagramLogin").Value,
+                Password = _configuration.GetSection("Credentials").GetSection("InstagramPassword").Value
+            };
+
+            var _instaApi = InstaApiBuilder.CreateBuilder()
+                   .SetUser(userSession)
+                   .UseLogger(new DebugLogger(InstaSharper.Logger.LogLevel.Exceptions)) // use logger for requests and debug messages
+                   .SetRequestDelay(delay)
+                   .Build();
+            if (!_instaApi.IsUserAuthenticated)
+            {
+                // login
+                Console.WriteLine($"Logging in as {userSession.UserName}");
+                delay.Disable();
+                var logInResult = await _instaApi.LoginAsync();
+                delay.Enable();
+                if (!logInResult.Succeeded)
+                {
+                    Console.WriteLine($"Unable to login: {logInResult.Info.Message}");
+                }
+            }
+            var result =_instaApi.GetUserAsync("philomelka");
+            var followers = await _instaApi.GetUserFollowersAsync("philomelka",
+                PaginationParameters.MaxPagesToLoad(5));
+            return new JsonResult(new { result = followers });
         }
     }
 }
